ef7bceae5c9d47287163d30466d8e82e85b89658
Rob Pliszka <robrails87@gmail.com>
2012-10-14 18:14:10 -0400

    Done with static pages fix

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,42 +3,45 @@ require 'spec_helper'
 describe PagesController do
   render_views
 
-  describe "GET 'home'" do
-    it "returns http success" do
+  
+ describe "GET 'home'" do
+    it "should be successful" do
       get 'home'
       response.should be_success
     end
     
     it "should have the right title" do
       get 'home'
-      response.should have_selectro("title", 
-                        :content => "Ruby on Rails Turtorial Sample App | Home")
+      response.should have_selector("title", 
+                        :content => "Ruby on Rails Tutorial Sample App | Home")
     end                 
   end
 
   describe "GET 'contact'" do
-    it "returns http success" do
+    it "should be successful" do
       get 'contact'
       response.should be_success
     end
     
     it "should have the right title" do
       get 'contact'
-      response.should have_selectro("title", 
-                        :content => "Ruby on Rails Turtorial Sample App | Contact")
+      response.should have_selector("title", 
+                        :content => 
+                          "Ruby on Rails Tutorial Sample App | Contact")
     end
   end
   
   describe "GET 'about'" do
-    it "returns http success" do
+    it "should be successful" do
       get 'about'
       response.should be_success
     end
     
     it "should have the right title" do
       get 'about'
-      response.should have_selectro("title", 
-                        :content => "Ruby on Rails Turtorial Sample App | About")
+      response.should have_selector("title", 
+                        :content => 
+                          "Ruby on Rails Tutorial Sample App | About")
     end
   end
 end
